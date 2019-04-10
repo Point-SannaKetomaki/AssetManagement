f703b73c102b651a270ee1abbc553751b47e72e9
Sanna Ketomaki <sanna.ketomaki@stud.pointcollege.fi>
2019-04-10 12:31:20 +0300

    Lisätty yksinkertainen toteutus laitteiden tallentamisesta tiettyyn paikkaan

---
+++ AssetManagementWeb/AssetManagementWeb/Models/AssetLocationModel.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace AssetManagementWeb.Models
+    // sisältää jsonin sisältämän datan
+{
+    public class AssetLocationModel
+    {
+        public string AssetCode { get; set; }
+        public string LocationCode { get; set; }
+    }
+}
