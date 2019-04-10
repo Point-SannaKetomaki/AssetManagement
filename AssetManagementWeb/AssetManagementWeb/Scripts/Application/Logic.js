d766a2b7c6a0084e27802434e8cdd8b8fcad1a0b
Sanna Ketomaki <sanna.ketomaki@stud.pointcollege.fi>
2019-04-10 21:07:02 +0300

    Korjattu virheet. Assetteja voi syöttää ja ne tallentuvat tietokantaan.

--- AssetManagementWeb/AssetManagementWeb/Scripts/Application/Logic.js
+++ AssetManagementWeb/AssetManagementWeb/Scripts/Application/Logic.js
@@ -1,7 +1,39 @@
 /// <reference path="../typings/jquery/jquery.d.ts" />
+/*Kopioitu AssetLocationModelista ja muokataan typescript muotoon
+ * public class AssetLocationModel {
+    public string AssetCode { get; set; }
+        public string LocationCode { get; set; }
+    }  ===>  */
+var AssetLocationModel = /** @class */ (function () {
+    function AssetLocationModel() {
+    }
+    return AssetLocationModel;
+}());
 function initAssetAssignment() {
     $("#AssignAssetButton").click(function () {
-        alert("Toimii!");
+        //alert("Toimii!");
+        var locationCode = $('#LocationCode').val();
+        var assetCode = $('#AssetCode').val();
+        alert('L: ' + locationCode + ', A: ' + assetCode);
+        var data = new AssetLocationModel();
+        data.LocationCode = locationCode;
+        data.AssetCode = assetCode;
+        //lähetetäänä json-muotoista dataa palvelimelle
+        $.ajax({
+            type: "POST",
+            url: "/Asset/AssetLocation",
+            data: JSON.stringify(data),
+            contentType: "application/json",
+            success: function (data) {
+                if (data.success == true) {
+                    alert("Asset succesfully assigned");
+                }
+                else {
+                    alert("Error occurred: " + data.error);
+                }
+            },
+            dataType: "json"
+        });
     });
 }
 //# sourceMappingURL=Logic.js.map
--- AssetManagementWeb/AssetManagementWeb/Scripts/Application/Logic.ts
+++ AssetManagementWeb/AssetManagementWeb/Scripts/Application/Logic.ts
@@ -1,7 +1,45 @@
 ﻿/// <reference path="../typings/jquery/jquery.d.ts" />
 
+/*Kopioitu AssetLocationModelista ja muokataan typescript muotoon
+ * public class AssetLocationModel {
+    public string AssetCode { get; set; }
+        public string LocationCode { get; set; }
+    }  ===>  */
+
+class AssetLocationModel {
+    public AssetCode: string;
+    public LocationCode: string;
+    }
+
+
 function initAssetAssignment() {
     $("#AssignAssetButton").click(function () {
-        alert("Toimii!");
+        //alert("Toimii!");
+
+        var locationCode: string = $('#LocationCode').val();
+        var assetCode: string = $('#AssetCode').val();
+
+        alert('L: ' + locationCode + ', A: ' + assetCode);
+
+        var data: AssetLocationModel = new AssetLocationModel();
+        data.LocationCode = locationCode;
+        data.AssetCode = assetCode;
+
+        //lähetetäänä json-muotoista dataa palvelimelle
+        $.ajax({
+            type: "POST",
+            url: "/Asset/AssetLocation",
+            data: JSON.stringify(data),
+            contentType: "application/json",
+            success: function (data) {
+                if (data.success == true) {
+                    alert("Asset succesfully assigned");
+                }
+                else {
+                    alert("Error occurred: " + data.error);
+                }
+            },
+            dataType: "json"
+        });
     });
 }
